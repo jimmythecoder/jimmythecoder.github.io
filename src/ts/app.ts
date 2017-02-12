b638f1071bd706d2d1f80e05740fb5545d83fdcb
James Harris <dev@jharris.eu>
2017-02-12 22:33:31 +1100

    d3 visualisation for skills Started mobile responsive design

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,4 +1,8 @@
 /// <reference path="../typings/skype.d.ts" />
+/// <reference path="../../node_modules/@types/d3/index.d.ts" />
+/// <reference path="../../node_modules/@types/d3-color/index.d.ts" />
+/// <reference path="../../node_modules/@types/d3-scale/index.d.ts" />
+/// <reference path="../../node_modules/@types/d3-hierarchy/index.d.ts" />
 
 namespace app {
 
@@ -24,6 +28,10 @@ namespace app {
 
 				 $('.inner',this).css({'width':  leftOffset, opacity: opacity});
 			 });
+
+			 let skillsChart = new SkillsChart();
+
+			 skillsChart.render();
 		}
 	}
 
@@ -31,6 +39,54 @@ namespace app {
 		constructor() {
 			window.location.href = 'mailto:\u006a\u0061\u006d\u0065\u0073\u0040\u006a\u0068\u0061\u0072\u0072\u0069\u0073\u002e\u006e\u007a';
 			return false;
+		}
+	}
+	
+
+	class SkillsChart {
+
+		constructor() {
+			// init
+		}
+
+		render() {
+
+			let margins = 2;
+			let width = 650;
+
+			let svg = d3.select("svg.skills-chart");
+			let g = svg.append("g").attr("transform", "translate(2,2)");
+			let format = d3.format(",d");
+
+			var pack = d3.pack()
+				.size([width - 4, width - 4]);
+
+			d3.json("js/skills.json", function(error: any, root: any) {
+
+				if (error) {
+					throw error;
+				}
+
+				root = d3.hierarchy(root)
+					.sum(function(d: any) { return d.size; })
+					.sort(function(a: any, b: any) { return b.value - a.value; });
+
+				var node = g.selectAll(".node")
+					.data(pack(root).descendants())
+					.enter().append("g")
+					.attr("class", function(d: any) { return d.children ? "node" : "leaf node"; })
+					.attr("transform", function(d: any) { return "translate(" + d.x + "," + d.y + ")"; });
+
+				node.append("title")
+					.text(function(d: any) { return d.data.name + "\n" + format(d.value); });
+
+				node.append("circle")
+					.attr("r", function(d: any) { return d.r - margins; });
+
+				node.filter(function(d: any) { return !d.children; }).append("text")
+					.attr("dy", "0.3em")
+					.text(function(d: any) { return d.data.name.substring(0, d.r / 3); });
+			});
 		}
 	}
 
